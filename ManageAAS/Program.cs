dc5b2e4e1b22d31a26be8f6ddf3a8eab6c4b232f
Kornel R <lenrok09@gmail.com>
2018-05-30 15:07:45 +0200

    first commit

---
+++ ManageAAS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AnalysisServices.Tabular;
+//using Microsoft.AnalysisServices.Core
+
+namespace ManageAAS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.AnalysisServices.Tabular.Server serv = new Server();
+            //var connstr = "Provider = MSOLAP; Data Source =; Initial Catalog =< aas database name>; User ID =< your username >; Password =< your password >
+            serv.Connect("asazure://westeurope.asazure.windows.net/korndemo2");
+            Database db = serv.Databases["TabularProject1"]; 
+            Model mod = db.Model;
+            mod.RequestRefresh(RefreshType.ClearValues);
+            mod.SaveChanges();
+            mod.RequestRefresh(RefreshType.Full);
+            mod.SaveChanges();
+        }
+    }
+}
